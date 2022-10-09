1654bf2b2649555e85d9db6a3f09a6a6a0e686ac
dwang88 <davidwang1089@gmail.com>
2022-10-09 11:16:42 -0700

    update ui

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, ScrollView, Image } from 'react-native';
+import { Text, View, StyleSheet, Button, ScrollView, Image, TouchableHighlight, Pressable } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as cheerio from 'cheerio';
 import Card from './styles/card';
@@ -46,7 +46,7 @@ export default function App() {
       const pre_filter = ingredients.product.ingredients_text.toLowerCase().replace(/\./g, '');
       const filter = pre_filter.split(", ");
 
-      const mesh = ["red 40", "soy lecithin", "red#40", "natural flavors", "natural and artificial flavors", "caramel color", "fully hydrogenated vegetable oils", "aluminum", "palm oil", "corn syrup", "fructose", "glucose", "yellow 5", "yellow 6", "blue 1", "concentrate", "xanthan gum", "lecithin", "artificial" ];
+      const mesh = ["red 40", "soy lecithin", "red#40", "natural flavors", "natural and artificial flavors", "caramel color", "fully hydrogenated vegetable oils", "aluminum", "palm oil", "corn syrup", "fructose", "glucose", "yellow 5", "yellow 6", "blue 1", "concentrate", "xanthan gum", "lecithin", "artificial", "monosodium glutamate", "acesulfame potassium" ];
       var filtered = [];
       setText(ingredients.product.ingredients_text);
       setText(pre_filter);
@@ -67,11 +67,27 @@ export default function App() {
             }
         }
     }
+
     setText4(filtered);
     }
     getData(api_url);
   };
 
+  const getBackgroundColor = () => {
+    let color;
+    if (text3 === 'C' || text3 === 'D' || text3 === 'E') {
+        console.log(text3)
+        color = 'red';
+        console.log(color);
+        return color;
+    } else if (text3 === 'A' || text3 === 'B') {
+      color = 'green';
+      console.log(color);
+      return color;
+    } else {
+      color = 'fff'
+    }
+};
 
   // Check permissions and return the screens
   if (hasPermission === null) {
@@ -97,7 +113,7 @@ export default function App() {
       <View style={styles.barcodebox}>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 600, width: 400 }} 
+          style={{ height: 900, width: 400 }} 
           />
       </View>
       <Text style={{textAlign: 'center'}}>{text2}</Text>
@@ -105,12 +121,28 @@ export default function App() {
         <Text style={styles.maintext}>{"Ingredients: " + text}</Text>
         </Card>
       <Card>
+      <View style={{backgroundColor: getBackgroundColor(), borderRadius: 6}}>
         <Text style={styles.text3}>{"Nutriscore: " + text3 + " (A-E)"}</Text>
-      </Card>  
+       </View>
+      </Card>
       <Card>
         <Text>{"Hazards: " + text4}</Text>
       </Card>
-      {scanned && <Button title={'Scan Again'} onPress={() => setScanned(false)} color='black' />}
+      <View>{scanned &&
+      <Pressable onPress={() => setScanned(false)} style={{backgroundColor: 'grey', borderRadius: 50, elevation: 3,
+        backgroundColor: '#F8F8F8',
+        shadowOffset: {width: 1, height: 1},
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        borderWidth: 4,
+        shadowRadius: 2,
+        marginHorizontal: 4,
+        marginVertical: 6,
+        padding: 18,
+        underlayColor: 'grey'
+        }}><Text style={{textAlign: 'center', fontSize: 16, fontWeight: 'bold'}}>SCAN</Text></Pressable>
+      }
+        </View>
     </View>
     </ScrollView>
   );
@@ -139,11 +171,15 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     overflow: 'hidden',
     borderRadius: 30,
-    backgroundColor: 'tomato'
+    backgroundColor: 'tomato',
+    marginTop: 30
   },
   text3: {
     textAlign: 'left',
     fontSize: 16,
     margin: 20,
+    fontWeight: 'bold',
   }
 });
+
+//<View style={{borderRadius: "60px"}}>{scanned && <Button title={'Scan Again'} onPress={() => setScanned(false)} style={{borderRadius: '60px'}} color='black' />}</View>
--- styles/card.js
+++ styles/card.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     card: {
         borderRadius: 6,
         elevation: 3,
-        backgroundColor: '#fff',
+        backgroundColor: '#F8F8F8',
         shadowOffset: {width: 1, height: 1},
         shadowColor: '#333',
         shadowOpacity: 0.3,
